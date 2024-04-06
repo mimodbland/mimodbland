504a3d2ba1c2567cfd442ea95ba1536d550311bd
mimodbland <mimodbland@gmail.com>
2024-04-06 09:39:20 +0200

    list add new repo local

--- first_file.py
+++ first_file.py
@@ -4,3 +4,28 @@ print('Hola Github desde remoto cuenta mimodbland@gmail.com')
 print('Hola Github desde local cuenta mimodbland@gmail.com')
 print('Hola Github desde remoto cuenta toviddfrei@gmail.com')
 print('Hola Github desde local cuenta toviddfrei@gmail.com')
+
+"""
+# Notes & Exemples:
+
++ Crear un directorio en cualquier lugar de nuestro equipo
+- Acceder al directorio nuevo creado
+- Ejecutar el comando git init
+- Devuelve mensaje del resultado de la ejecucion del comando
+- Comprobar el resultado
+- Ejecutar el comando ls -la
+- Devuelve mensaje listado de todos los directorios y archivos 
+- Ver que se ha creado el directorio .git
+- Comprobar el estado de git
+- Ejecutar comando git status
+- Devuelve mensaje rama en la que estamos y la situación que tiene
+
+
+# Commands
+
+- git init -- activar git en un directorio
+- ls -la -- listar todos los archivos y directorios
+- git status -- comprobar el estado de git 
+"""
+
+
